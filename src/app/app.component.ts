e2e906a0a07d90afee0137a65e88afe35f9bbb03
Danny <mtme13@hotmail.com>
2020-02-10 14:43:32 +0000

    Add FadeInOut amnimation for list of items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import {
+  style,
+  state,
+  transition,
+  trigger,
+  animate
+} from "@angular/animations";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
+  animations: [
+    trigger("fadeInOut", [
+      state(
+        "void",
+        style({
+          opacity: "0",
+          transform: "translateX(40px)"
+        })
+      ),
+      state(
+        "show",
+        style({
+          opacity: "1"
+        })
+      ),
+      transition("void <=> *", [animate(".5s")])
+    ])
+  ]
 })
 export class AppComponent {
-  title = 'angular-animations';
+  title = "angular-animations";
+  list = [];
+
+  addListItem() {
+    const item = { title: "abc", body: "Hello chum" };
+    this.list.push(item);
+  }
+
+  removeListItem() {
+    this.list.pop();
+  }
 }
