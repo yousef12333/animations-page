ae23b1cfcea511cb5edcdd43893a655c426de7ed
Danny <mtme13@hotmail.com>
2020-02-13 10:06:15 +0000

    Refactor to create reusable fadeInX  function

--- src/app/feature/animation/animations/fading-entrance-animations.ts
+++ src/app/feature/animation/animations/fading-entrance-animations.ts
@@ -2,11 +2,15 @@ import { animation, animate, keyframes, style } from "@angular/animations";
 
 const DEFAULT_TIME = "1s";
 
-export const FADE_IN_ANIMATION =
-  animation([
+// add parameters to params object to use in animation
+export function fadeInX(startX, endX) {
+  return animation(
     animate("{{ time }}", keyframes([
-      style({ opacity: "1" }),
-      style({ opacity: "0" }),
-      style({ opacity: "1" })
-    ]))
-  ], { params: { time: DEFAULT_TIME } })
+      style({ opacity: "0", transform: "translate3d({{ startX }}, 0, 0)" }),
+      style({ opacity: "1", transform: "translate3d({{ endX }}, 0, 0)" })
+    ])),
+    { params: { time: DEFAULT_TIME, startX, endX } }
+  )
+}
+
+export const FADE_IN_ANIMATION = fadeInX(0, 0);
