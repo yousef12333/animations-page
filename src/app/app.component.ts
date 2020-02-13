b082aef43054e50567b3c0bcb65365dc6b1948a0
Danny <mtme13@hotmail.com>
2020-02-13 13:47:25 +0000

    Add super basic fadeIn route animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,15 +4,27 @@ import {
   state,
   transition,
   trigger,
-  animate
+  animate,
+  useAnimation
 } from "@angular/animations";
+import { RouterOutlet } from '@angular/router';
+import { FADE_IN_ANIMATION } from "./feature/animation/animations/fading-entrance-animations";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"]
+  styleUrls: ["./app.component.scss"],
+  animations: [
+    trigger("fadeIn", [transition("* <=> *", useAnimation(FADE_IN_ANIMATION))])
+  ]
 })
 export class AppComponent {
   pageTitle = "Angular Animations";
   animationName = "Fade In-Out";
+
+  prepareRoute(outlet: RouterOutlet) {
+    return outlet && outlet.activatedRouteData
+
+    // && outlet.activatedRouteData['animation']
+  }
 }
